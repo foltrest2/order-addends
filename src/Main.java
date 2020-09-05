47d8bb38bd784967723760be7cb45bd826e5947c
Julian Riascos <julian.riascos1@correo.icesi.edu.co>
2020-09-04 20:48:27 -0500

    Se buggeo la anterior, esta es la buena xd

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,70 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-//		Reading input
-		String line = br.readLine();
-		String[] numsLine = line.split("\\+");
-		int[] nums = new int [numsLine.length];
-		
-//		Cast values to int
-		for (int i = 0; i < nums.length; i++) {
-			nums[i] = Integer.parseInt(numsLine[i]);
-		}
-		
-//		Sorting
-		for (int i = nums.length-1; i > 0; i--) {
-			for (int j = 0; j < i; j++) {
-				if (nums[j] > nums[j+1]) {
-					int temp  = nums[j];
-					nums[j]   = nums[j+1];
-					nums[j+1] = temp;
-				}
-			}
-		}
-		
-//		Output
-		String output = "";
-		for (int i = 0; i < nums.length-1; i++) {
-			output += nums[i] + "+";
-		}
-		
-		output += nums[nums.length-1];
-		
-		bw.write(output);
-		
-		br.close();
-		bw.close();
-	}
 
+		int cantLines = Integer.parseInt(br.readLine());  
+		
+		for(int i=1; i<=cantLines; i++) { 
+			String line = "";
+			line = br.readLine();
+			String[] change = line.split(" ");
+			double[] preOrganized = new double[change.length];	
+			for (int j = 0; j < preOrganized.length; j++) {
+				preOrganized[j] = Double.parseDouble(change[j]);
+			}
+			
+			int moves = 0;
+			int[] preAverage = new int[preOrganized.length-1];
+			for (int k = preOrganized.length-1; k > 0; k--) {
+				for (int j = 0; j < k; j++) {
+					if(preOrganized[j] > preOrganized[j+1]) {
+						double temp = preOrganized[j];
+						preOrganized[j] = preOrganized[j+1];
+						preOrganized[j+1] = temp;
+						moves++;
+					}
+				}
+				preAverage[k-1] = moves;
+				moves = 0;
+			}
+			
+			double sum = 0;
+			for (int j = 0; j < preAverage.length; j++) {	
+				sum += preAverage[j];	
+			}
+			
+			double average = 0;
+			average = sum / (double)preAverage.length;
+
+//			
+//			
+			BigDecimal bd = new BigDecimal(average);
+			bd = bd.setScale(2, RoundingMode.DOWN);
+			String formatNumberS = bd.toString(); 
+			String s = formatNumberS.indexOf(".") < 0 ? formatNumberS : formatNumberS.replaceAll("0*$", "").replaceAll("\\.$", ".0");
+//			
+//			The idea of the line 56 was obtained of this links
+//			https://es.stackoverflow.com/questions/29408/como-limitar-la-cantidad-de-decimales-de-un-double
+//			https://stackoverflow.com/questions/14984664/remove-trailing-zero-in-java
+			
+			String organized = "";
+			organized += s + "-";
+			for (int j = 0; j < preOrganized.length-1; j++) {
+				organized += preOrganized[j] + " "; 
+			}
+			organized += preOrganized[preOrganized.length-1];
+			bw.write(organized+"\n");
+		}
+		bw.close();
+		br.close();
+	}
 }
